92fb996c9aa0acb461495080ffae958d79efeac3
MissDine <enidnyatichi1212@gmail.com>
2022-06-09 15:42:29 +0300

    Feature edit

--- oop.rb
+++ oop.rb
@@ -1,6 +1,10 @@
 # An object in code is a thing with all the data and all the logic required to complete a task. Objects are models and metaphors for the problems we solve in code
 # Class is a custom data type eg Book.... all classes must be an object hence variable name starts with a capital letter
 # Instance of a class is an object eg book 1 and book 2
+# Objects are instances of a class
+# Class names start with capital letters because they are stored as constatnts in ruby, they can also be camelCased
+# n instance is a single occurrence of an object. 
+# Instances refer to the individual objects produced from the class.
 puts 1.class
 # Everything in a ruby 
 puts 2.is_a?Object
@@ -27,5 +31,22 @@ book2.author = "Ken Walibora"
 
 puts book2.author
 
+# Initialize method
+class Names
+    attr_accessor :titles, :lugha
+    def initialize (titles,lugha)
+        @titles = titles,
+        @lugha = lugha   
+    end 
+end 
 
+name1 = Names.new("Enid","Kisii")
+
+name2 = Names.new("Norbert","Ekegusii")
+      
+puts name2.lugha
+
+puts name1.class
+# ruby methods of classes
+# puts name1.methods
 
