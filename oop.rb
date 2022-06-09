126e6722811e5d9c50720afaa7925a60944df716
MissDine <enidnyatichi1212@gmail.com>
2022-06-09 14:58:59 +0300

    Feature edit

---
+++ oop.rb
@@ -0,0 +1,31 @@
+# An object in code is a thing with all the data and all the logic required to complete a task. Objects are models and metaphors for the problems we solve in code
+# Class is a custom data type eg Book.... all classes must be an object hence variable name starts with a capital letter
+# Instance of a class is an object eg book 1 and book 2
+puts 1.class
+# Everything in a ruby 
+puts 2.is_a?Object
+p "Enid".is_a?Object
+
+puts [1,2,3].class
+
+class Book
+    attr_accessor :title, :author, :pages
+end    
+
+book1 = Book.new()
+book1.title = "The river and the source"
+book1.author = "Margret Ogolla"
+book1.pages = 1000
+
+puts book1.title
+puts book1.pages
+
+book2 = Book.new
+book2.title = "Kidagaa kimemwozea"
+book2.pages = 300
+book2.author = "Ken Walibora"
+
+puts book2.author
+
+
+
